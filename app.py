c9c606d75250a3872408af109e91800c343341f6
lukaj <luka.jovanovski@hotmail.com>
2022-04-22 03:36:37 +0200

    Fully functional REST api

--- app.py
+++ app.py
@@ -1,33 +1,107 @@
-from functools import wraps
-
 import connexion
-from flask import request, abort
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 
+def get_students():
+    students = db.session.query(Student).all()
+    return [x.json() for x in students]
+
+
 def student_add(student_body):
-    new_student = Student(name=student_body['name'])
+    new_student = Student(name=student_body['name'], surname=student_body['surname'])
     db.session.add(new_student)
     db.session.commit()
+    return new_student.json()
 
 
-def student_find(student_id):
-    found_person = db.session.query(Student).filter_by(id=student_id).first()
-    if found_person:
-        return found_person
+def find_student(student_id):
+    found_student = query_student(student_id)
+    if found_student:
+        return found_student.json()
     else:
         return {'error': '{} not found'.format(student_id)}, 404
 
 
-def course_add(course_body):
-    return "test"
-    # student = student_find(course_body['student_id'])
-    # new_course = Course(name=course_body['name'])
-    # student.courses.append(new_course)
-    # db.session.add(student)
-    # db.session.add(new_course)
-    # db.session.commit()
+def edit_student(student_id, student_body):
+    student = query_student(student_id)
+    if student:
+        for property_name, value in student_body.items():
+            setattr(student, property_name, value)
+        db.session.add(student)
+        db.session.commit()
+        return student.json()
+    else:
+        return {'error': '{} not found'.format(student_id)}, 404
+
+
+def delete_student(student_id):
+    student = query_student(student_id)
+    if student:
+        response = student.json()
+        Student.query.filter_by(id=student_id).delete()
+        db.session.commit()
+        return response
+    else:
+        return {'error': '{} not found'.format(student_id)}, 404
+
+
+def get_courses():
+    courses = db.session.query(Course).all()
+    return [x.json() for x in courses]
+
+
+def find_course(course_id):
+    course = db.session.query(Course).filter_by(id=course_id).first()
+    if course:
+        return course.json()
+    else:
+        return {'error': '{} not found'.format(course_id)}, 404
+
+
+def add_course(student_id, course_body):
+    student = query_student(student_id)
+    new_course = Course(name=course_body['name'], grade=course_body['grade'])
+    student.courses.append(new_course)
+    db.session.add(new_course)
+    db.session.commit()
+    return new_course.json()
+
+
+def edit_course(course_id, course_body):
+    course = query_course(course_id)
+    if course:
+        course = update_model(course, course_body)
+        return course.json()
+    else:
+        return {'error': '{} not found'.format(course_id)}, 404
+
+
+def delete_course(course_id):
+    course = query_course(course_id)
+    if course:
+        response = course.json()
+        Course.query.filter_by(id=course_id).delete()
+        db.session.commit()
+        return response
+    else:
+        return {'error': '{} not found'.format(course_id)}, 404
+
+
+def query_student(student_id):
+    return Student.query.filter_by(id=student_id).first()
+
+
+def query_course(course_id):
+    return Course.query.filter_by(id=course_id).first()
+
+
+def update_model(model, upodate_params):
+    for property, value in upodate_params.items():
+        setattr(model, property, value)
+    db.session.add(model)
+    db.session.commit()
+    return model
 
 
 connexion_app = connexion.App(__name__, specification_dir="./")
--- models.py
+++ models.py
@@ -7,11 +7,18 @@ class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
     surname = db.Column(db.String(128))
-    courses = relationship("Course")
+    courses = relationship("Course", cascade='all, delete, delete-orphan')
+
+    def json(self):
+        return {'id': self.id, 'name': self.name, 'surname': self.surname, 'courses': [x.json() for x in self.courses]}
 
 
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
-    student_id = db.Column(db.Integer, db.ForeignKey("student.id"))
+    grade = db.Column(db.Integer)
+    student_id = db.Column(db.Integer, db.ForeignKey("student.id", ondelete="CASCADE"))
     student = relationship("Student", back_populates="courses")
+
+    def json(self):
+        return {'id': self.id, 'name': self.name, 'grade': self.grade}
